518030dba68bce4592c031355c792f3f9e61c1a7
Paniraj Koppa <paniraj.km@gmail.com>
2021-08-17 16:33:00 -0700

    first commit

---
+++ resources.tf
@@ -0,0 +1,27 @@
+/*
+resource "netapp-cloudmanager_cvo_aws" "cvo-aws" {
+  #provider = netapp-cloudmanager
+  name   = var.name_of_cvo_working_environment
+  region = var.region
+  subnet_id = var.subnet
+  svm_password = var.cvo_admin_password
+  client_id           = var.connector_id
+  writing_speed_state = "NORMAL"
+}
+*/
+
+
+
+resource "netapp-cloudmanager_volume" "cvo-volume-nfs" {
+  provider = netapp-cloudmanager
+  volume_protocol = "nfs"
+  name = "volume-1"
+  size = 1
+  unit = "GB"
+  provider_volume_type = "gp2"
+  export_policy_type = "custom"
+  export_policy_ip = ["0.0.0.0/0"]
+  export_policy_nfs_version = ["nfs4"]
+  working_environment_id = var.working_environment_id
+  client_id = var.connector_id
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "cloudmanager_refresh_token" {
+}
+
+
+variable "cvo_admin_password" {
+}
+
+
+variable "name_of_cvo_working_environment" {
+}
+
+
+variable "region" {
+}
+
+
+variable "subnet" {
+}
+
+
+variable "connector_id" {
+}
+
+
+variable "working_environment_id" {
+}
